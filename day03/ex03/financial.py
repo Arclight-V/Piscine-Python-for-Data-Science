226c6b6c0861a1e1222867b9ab5f89ce78be7acf
OUT-Kalanchin-VV <VVKalanchin@sberbank@mail.com>
2021-11-05 13:56:31 +0300

    ex03 day03 check for virtual env

--- day03/ex03/financial.py
+++ day03/ex03/financial.py
@@ -1,12 +1,23 @@
 import sys
+import os
+
+def financial():
+    x = None
 
 if __name__ == '__main__':
+    name_env = 'anatashi'
     red = '\033[31m'
     blue = '\033[34m'
     reset = '\033[0m'
+
     try:
+        if os.environ['VIRTUAL_ENV'][-len(name_env):] != name_env:
+            raise KeyError('VIRTUAL_ENV != {}', name_env)
         if len(sys.argv) != 2:
             raise Exception(f"Must by there should be 2 arguments: ticker symbol and the field of the table\n{blue}"
                             f"Example{reset}: financial.py 'MSFT' 'Total Revenue'")
+        financial()
     except Exception as err:
         print(f'{red}Arguments Error{reset}: {err}')
+    except KeyError as kerr:
+        print(f'KeyError: {kerr} is None')
