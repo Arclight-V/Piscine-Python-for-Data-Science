87f445bb13d74283d7104295ead3a7c4c1e0db34
OUT-Kalanchin-VV <VVKalanchin@sberbank@mail.com>
2021-11-05 12:03:49 +0300

    ex02 day03 complited

--- day03/ex02/librarian.py
+++ day03/ex02/librarian.py
@@ -1,10 +1,17 @@
 import os
+import shutil
 
-def librarian():
+def librarian(name_env):
     list_lib = ['beautifulsoup4',
-                'pytest']
-    #
-    # print (' '.join(list_lib))
+                'pytest',
+                ]
+    pip_inst = 'pip3 install '
+
+    os.system(pip_inst + ' '.join(list_lib))
+    os.system('pip freeze > requirements.txt')
+
+    shutil.make_archive(name_env, 'zip', name_env)
+
 
 if __name__ == '__main__':
     name_env = 'anatashi'
@@ -12,6 +19,6 @@ if __name__ == '__main__':
         if os.environ['VIRTUAL_ENV'][-len(name_env):] != name_env:
             raise KeyError('VIRTUAL_ENV != {}', name_env)
         else:
-            librarian()
+            librarian(name_env)
     except KeyError as kerr:
         print(f'KeyError: {kerr} is None')
