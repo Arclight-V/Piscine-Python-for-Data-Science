bfb701c872e03a9c275945426e1f7121a20d9dee
OUT-Kalanchin-VV <VVKalanchin@sberbank@mail.com>
2021-11-06 10:58:28 +0300

    ex00 day04 complited

--- day04/ex00/benchmark.py
+++ day04/ex00/benchmark.py
@@ -11,6 +11,10 @@ def loop(email, src_emails):
     return new_list_emails
 
 
+def list_comprehension(email, src_emails):
+    return [email for email in src_emails]
+
+
 if __name__ == '__main__':
     emails = ['john@gmail.com',
               'james@gmail.com',
@@ -21,5 +25,12 @@ if __name__ == '__main__':
     number_of_repetitions = 90000000
 
     loop_function = 'loop(emails[5], emails)'
+    comprehension_function = 'list_comprehension(emails[5], emails)'
 
-    print(timeit.timeit(loop_function, globals=globals(), number=number_of_repetitions))
+    timeit_loop = timeit.timeit(loop_function, globals=globals(), number=number_of_repetitions)
+    timeit_comprehension = timeit.timeit(loop_function, globals=globals(), number=number_of_repetitions)
+
+    if (timeit_comprehension <= timeit_loop):
+        print(f'it is better to use a list comprehension\n{timeit_comprehension} vs {timeit_loop}')
+    else:
+        print(f'it is better to use a loop\n{timeit_loop} vs {timeit_comprehension}')
