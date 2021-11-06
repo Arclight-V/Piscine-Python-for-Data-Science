e2ba347e430b556247fe3338c184f705a8e00aff
OUT-Kalanchin-VV <VVKalanchin@sberbank@mail.com>
2021-11-06 11:46:14 +0300

    ex00 day04 add find at email

--- day04/ex00/benchmark.py
+++ day04/ex00/benchmark.py
@@ -3,16 +3,16 @@
 import timeit
 
 
-def loop(email, src_emails):
+def loop(atemail, src_emails):
     new_list_emails = []
     for elem in src_emails:
-        if elem == email:
+        if elem.endswith(atemail):
             new_list_emails.append(elem)
     return new_list_emails
 
 
-def list_comprehension(email, src_emails):
-    return [email for email in src_emails]
+def list_comprehension(atemail, src_emails):
+    return [atemail for atemail in src_emails if atemail.endswith(atemail)]
 
 
 if __name__ == '__main__':
@@ -23,14 +23,15 @@ if __name__ == '__main__':
               'philipp@gmail.com'] * 5
 
     number_of_repetitions = 90000000
+    atgmail = '@gmail.com'
 
-    loop_function = 'loop(emails[5], emails)'
-    comprehension_function = 'list_comprehension(emails[5], emails)'
+    loop_function = 'loop(atgmail, emails)'
+    comprehension_function = 'list_comprehension(atgmail, emails)'
 
     timeit_loop = timeit.timeit(loop_function, globals=globals(), number=number_of_repetitions)
     timeit_comprehension = timeit.timeit(loop_function, globals=globals(), number=number_of_repetitions)
 
-    if (timeit_comprehension <= timeit_loop):
+    if timeit_comprehension <= timeit_loop:
         print(f'it is better to use a list comprehension\n{timeit_comprehension} vs {timeit_loop}')
     else:
         print(f'it is better to use a loop\n{timeit_loop} vs {timeit_comprehension}')
